3cedac1f8aeb935d1809daeddddf333cb2c21a10
Noe001 <goodtack530@gmail.com>
2024-12-31 23:07:32 +0900

    backend側修正

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -1,9 +1,2 @@
 class ApplicationController < ActionController::API
-  rescue_from StandardError, with: :handle_error
-
-  private
-
-  def handle_error(e)
-    render json: { error: e.message }, status: :internal_server_error
-  end
 end
--- backend/db/migrate/20241226144015_create_users.rb
+++ backend/db/migrate/20241226144015_create_users.rb
@@ -1,7 +1,7 @@
 class CreateUsers < ActiveRecord::Migration[7.2]
   def change
     create_table :users, id: false do |t|
-      t.string :id, limit: 36, null: false, primary_key: true
+      t.string :id, limit: 36, null: false, primary_key: true, default: -> { "uuid()" }
       t.string :name
       t.string :email
       t.string :password_digest
