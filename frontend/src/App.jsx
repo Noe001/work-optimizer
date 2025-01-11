61b26b14d51a638731b19aa1cf436a51e1d00802
Noe001 <goodtack530@gmail.com>
2025-01-11 18:34:30 +0900

    バグ修正

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import DashboardView from './pages/general/Dashboard';
+import ManualView from './pages/general/Manual';
 
 const App = () => (
   <Router>
     <Routes>
       <Route path="/" element={<DashboardView />} />
+      <Route path="/manual" element={<ManualView />} />
     </Routes>
   </Router>
 );
