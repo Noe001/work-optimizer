50dd963927eeb2f892435e96c101f9a486858eaa
Noe001 <goodtack530@gmail.com>
2025-02-07 00:11:35 +0900

    loginページ作成

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import DashboardView from './pages/general/Dashboard';
 import CreateManualView from './pages/general/CreateManual';
+import LoginView from './pages/login';
 
 const App = () => (
   <Router>
     <Routes>
       <Route path="/" element={<DashboardView />} />
       <Route path="/manual" element={<CreateManualView />} />
+      <Route path="/login" element={<LoginView />} />
     </Routes>
   </Router>
 );
